ff2131ff2a5a565734277084ca32f829524fab8d
zouhuajian <zouhuajianclever@gmail.com>
2018-06-17 17:40:24 +0800

    test pull request

--- src/main/resources/public/js/cluster-list-manager.js
+++ src/main/resources/public/js/cluster-list-manager.js
@@ -144,7 +144,6 @@ $("body").delegate(".delete-container","click", function(){
                             if(code == 0) {
                                  deleteObj.remove();
                                  layer.msg("delete " + clusterName + " successfully")
-                                 /*setTimeout("location.reload()", 1000);*/
                             } else {
                                 layer.msg("delete " + clusterName + " failed")
                             }
