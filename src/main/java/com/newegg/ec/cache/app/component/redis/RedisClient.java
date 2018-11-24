40d591b0973e539140981bdf121af333debec0eb
lf52 <Leo.J.Fu@newegg.com>
2018-11-24 11:25:10 +0800

    update redisclient for config rewrite

---
+++ src/main/java/com/newegg/ec/cache/app/component/redis/RedisClient.java
@@ -0,0 +1,72 @@
+package com.newegg.ec.cache.app.component.redis;
+
+import com.newegg.ec.cache.core.logger.CommonLogger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+/**
+ * Created by Leo Fu on 2018/11/24.
+ *
+ * jedis客户端没有提供某些命令的API(如config rewrite等)，可以依据RESP协议简单实现这些的API
+ * 开箱即用，用完关闭客户都即可
+ */
+public class RedisClient {
+
+    public static final CommonLogger logger = new CommonLogger(RedisClient.class);
+
+    private Socket socket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+
+    public RedisClient(String host, int port){
+        try {
+
+            this.socket = new Socket(host,port);
+            this.outputStream = this.socket.getOutputStream();
+            this.inputStream = this.socket.getInputStream();
+            socket = new Socket();
+            socket.setReuseAddress(true);
+            socket.setTcpNoDelay(true);
+            socket.setSoLinger(true, 0);
+            socket.connect(new InetSocketAddress(host, port), 3000);
+            outputStream = socket.getOutputStream();
+            inputStream = socket.getInputStream();
+
+        } catch (IOException e) {
+            logger.error("Create RedisClient Error",e);
+        }
+    }
+
+    public String redisCommandOpt(String command) throws IOException {
+
+        byte[] bytes= new byte[4096];
+        outputStream.write(command.toString().getBytes());
+        inputStream.read(bytes);
+        return new String(bytes);
+
+    }
+
+    public void closeClient() {
+
+        try {
+            if(socket != null && !socket.isClosed()){
+                socket.close();
+            }
+            if(outputStream != null ){
+                outputStream.close();
+            }
+            if(inputStream != null ){
+                inputStream.close();
+            }
+        }catch (Exception e){
+            logger.error("Close RedisClient Error",e);
+        }
+
+    }
+
+
+}
