ae5b53a65f0a58cd4fd4a592158107feed95db16
Reason.H.Duan <Reason.H.Duan@newegg.com>
2018-12-12 17:18:09 +0800

    add build sh

---
+++ bin/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mvn clean package -Dmaven.test.skip=true
+rm -rf build
+mkdir build
+cd ./build
+mkdir logs
+mkdir conf
+cp -f ../target/redis-manager*.jar ./
+cp -rf ../target/lib ./
+cp -rf ../bin/st*.sh ./bin/
+cp -rf ../target/classes/* ./conf
+echo "please update the config."
+
+
+
