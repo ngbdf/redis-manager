5602dce7ab3910d04f4dcd77c4e086ee07673223
zouhuajian <zouhuajianclever@gmail.com>
2019-07-17 22:53:32 +0800

    clear and init

---
+++ redis-manager-dashboard/src/main/java/com/newegg/ec/redis/RedisManagerApplication.java
@@ -0,0 +1,19 @@
+package com.newegg.ec.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * start
+ *
+ * @author Jay.H.Zou
+ * @date 2019/7/17
+ */
+@SpringBootApplication
+public class RedisManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisManagerApplication.class);
+    }
+
+}
