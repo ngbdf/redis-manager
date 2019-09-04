82cddc6c8bb8ee80a8de3a10d2b55ae95293cc13
zouhuajian <zouhuajianclever@gmail.com>
2019-09-04 23:05:59 +0800

    index

--- redis-manger-ui/redis-manager-vue/src/components/Index.vue
+++ redis-manger-ui/redis-manager-vue/src/components/Index.vue
@@ -2,18 +2,33 @@
   <el-container id="index">
     <el-header class="header">
       <el-row type="flex" class="header-wrapper" justify="space-between">
-        <el-col :span="3">
+        <el-col class="logo-wrapper">
+          <div class="grid-content bg-purple logo">REDIS MANAGER</div>
+        </el-col>
+        <el-col>
+          <div class="grid-content bg-purple-light right-content">
+            <el-select v-model="value" placeholder="Select Group" size="small">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
+
+            <div class="user-info">
+              <el-avatar :size="36" src="../assets/logo.png" @error="errorHandler">
+                <img src="../assets/logo.png" />
+              </el-avatar>
+            </div>
+          </div>
+        </el-col>
+        <!-- <el-col :span="3" class="user-info">
           <div class="grid-content bg-purple"></div>
-        </el-col>
-        <el-col :span="18">
-          <div class="grid-content bg-purple-light"></div>
-        </el-col>
-        <el-col :span="3">
-          <div class="grid-content bg-purple"></div>
-        </el-col>
+        </el-col>-->
       </el-row>
     </el-header>
-    <el-container class>
+    <el-container class="aside-main-wrapper">
       <el-aside width="200px">
         <el-row class="tac">
           <el-col :span="24">
@@ -67,12 +82,30 @@
 
 <script>
 export default {
+  data() {
+    return {
+      options: [
+        {
+          value: "Bigdata",
+          label: "Bigdata"
+        },
+        {
+          value: "Test",
+          label: "Test"
+        }
+      ],
+      value: ""
+    };
+  },
   methods: {
     handleOpen(key, keyPath) {
       console.log(key, keyPath);
     },
     handleClose(key, keyPath) {
       console.log(key, keyPath);
+    },
+    errorHandler() {
+      return true;
     }
   }
 };
@@ -83,9 +116,35 @@ export default {
   height: 100%;
 }
 
+.header {
+  padding: 0;
+  line-height: 40px;
+}
 .header-wrapper {
   padding: 10px 0;
+  min-width: 600px;
 }
+
+.logo-wrapper {
+  width: 200px;
+  min-width: 200px;
+}
+
+.logo {
+  padding-left: 20px;
+}
+
+.right-content {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.user-info {
+  display: flex;
+  align-items: center;
+  margin: 0 20px;
+}
+
 /*页面下拉，header固定不动*/
 .header {
   /* position: fixed; */
@@ -100,36 +159,27 @@ export default {
   border-bottom: 1px solid #dcdfe6;
 }
 
-.aside-main,
-.aside {
-}
-
 .main {
   background-color: #f1f2f7;
+  min-width: 400px;
 }
 
-.el-row {
-  margin-bottom: 20px;
-  &:last-child {
-    margin-bottom: 0;
-  }
-}
-.el-col {
-  border-radius: 4px;
-}
 .bg-purple-dark {
   background: #99a9bf;
 }
+
 .bg-purple {
   background: #d3dce6;
 }
+
 .bg-purple-light {
   background: #e5e9f2;
 }
+
 .grid-content {
-  border-radius: 4px;
-  min-height: 36px;
+  min-height: 40px;
 }
+
 .row-bg {
   background-color: #f9fafc;
 }
