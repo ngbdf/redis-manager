17f4398bd4b44bffe015fcfa328d027a90c7fc69
zouhuajian <zouhuajianclever@gmail.com>
2019-09-17 22:55:23 +0800

    api

--- redis-manger-ui/redis-manager-vue/src/main.js
+++ redis-manger-ui/redis-manager-vue/src/main.js
@@ -8,13 +8,22 @@ import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css'
 import '../theme/index.css'
 import './assets/icon/iconfont.css'
+import axios from 'axios'
+import vuex from 'vuex'
 
 Vue.use(ElementUI, { locale })
 Vue.config.productionTip = false
 
+Vue.prototype.$axios = axios
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
+// axios.defaults.baseURL = 'http://localhost:8000/api/'
+// axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
+// axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  vuex,
   router,
   components: { App },
   template: '<App/>'
