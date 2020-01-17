aacf90c1d00ae77142415ade90358250a09fbd8a
ay05 <Aiden.Q.Yu@newegg.com>
2020-01-17 14:44:27 +0800

    添加放行eureka 静态资源配置

--- redis-manager-dashboard/src/main/java/com/newegg/ec/redis/controller/security/WebSecurityConfig.java
+++ redis-manager-dashboard/src/main/java/com/newegg/ec/redis/controller/security/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.http.HttpServletRequest;
@@ -55,6 +56,11 @@ public class WebSecurityConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/logo.ico");
         // 拦截配置
         addInterceptor.addPathPatterns("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/eureka/**").addResourceLocations("classpath:/static/eureka/");
     }
 
     public class SecurityInterceptor implements HandlerInterceptor {
