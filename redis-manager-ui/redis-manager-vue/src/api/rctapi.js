ae041db6c57c683dd99c5f7a407c134627ef16fa
Lola.Gou <18291003116@163.com>
2020-01-17 15:57:32 +0800

    封装api

--- redis-manager-ui/redis-manager-vue/src/api/rctapi.js
+++ redis-manager-ui/redis-manager-vue/src/api/rctapi.js
@@ -1,45 +1,81 @@
 import axios from 'axios'
 import message from '@/utils/message.js'
-import router from '@/router'
-import { store } from '@/vuex/store.js'
 
-// function ajax (url, data = {}, type = 'GET') {
-//   return new Promise((resolve, reject) => {
-//     let promise
-//     if (type === 'GET') {
-//       promise = axios.get(url, { params: data })
-//     } else {
-//       promise = axios.post(url, data)
-//     }
-//     promise.then(response => {
-//       resolve(response)
-//     }).catch(error => {
-//       message.error(error.message)
-//     })
-//   })
-// }
-
-function ajax (url, params = {}, method = 'GET') {
+function ajax (url, data = {}, type = 'GET') {
   return new Promise((resolve, reject) => {
-    axios({
-      method: method,
-      url: url,
-      headers: {'UserIp': store.getters.getUserIp},
-      data: method === 'POST' || method === 'PUT' ? params : null,
-      params: method === 'GET' || method === 'DELETE' ? params : null
-      // baseURL: root,
-      // withCredentials: true //开启cookies
-    }).then(res => {
-      resolve(res)
-    }).catch(err => {
-      if (err.response.status === 401) {
-        message.warn('Session timeout')
-        router.push({ name: 'login' })
-      } else {
-        message.error(err.message)
-      }
+    let promise
+    if (type === 'GET') {
+      promise = axios.get(url, { params: data })
+    } else {
+      promise = axios.post(url, data)
+    }
+    promise.then(response => {
+      resolve(response)
+    }).catch(error => {
+      reject(error)
+      message.error(error.message)
     })
   })
 }
 
-export const getDemo = (username, pass) => ajax('/test', {username, pass})
+if (process.env.NODE_ENV === 'development') {
+  axios.defaults.baseURL = '/apis'
+} else if (process.env.NODE_ENV === 'production') {
+  axios.defaults.baseURL = '/'
+}
+
+const service = axios.create({
+  timeout: 10000
+})
+
+service.interceptors.response.use(
+  response => {
+    const res = response.data
+    if (res.code !== 0) {
+      message.error(res.message || 'error')
+      return Promise.reject(new Error(res.message || 'error'))
+    } else {
+      return Promise.resolve(res)
+    }
+  },
+  error => {
+    // if (error.response.code === 401) {
+    //   message.warning('Session timeout')
+    //   router.push({ name: 'login' })
+    // } else {
+    //   return Promise.reject(error)
+    // }
+    return Promise.reject(error)
+  }
+)
+
+export const getAnalysisList = () => {
+  return service({
+    url: `/rdb`,
+    method: 'get'
+
+  })
+}
+
+export const addAnalysisList = (data) => {
+  return service({
+    url: `/rdb`,
+    method: 'post',
+    data
+  })
+}
+
+export const updateAnalyzeList = (data) => {
+  return service({
+    url: `/rdb`,
+    method: 'put',
+    data
+  })
+}
+
+export const deletAnalyze = (id) => {
+  return service({
+    url: `/rdb?id=${id}`,
+    method: 'delete'
+  })
+}
