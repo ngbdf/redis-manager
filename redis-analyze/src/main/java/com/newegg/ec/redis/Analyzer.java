8de20a75d9c61cc979617db5c9a0f0b7f52fa3fa
jc1e <jc1e@WXMIS080.buyabs.corp>
2020-02-13 11:41:13 +0800

    调整获取环境变量参数值方法

--- redis-analyze/src/main/java/com/newegg/ec/redis/Analyzer.java
+++ redis-analyze/src/main/java/com/newegg/ec/redis/Analyzer.java
@@ -1,6 +1,5 @@
 package com.newegg.ec.redis;
 import com.newegg.ec.redis.base.ElasticSearchUtil;
-import com.newegg.ec.redis.constant.AnalyzerConstant;
 import com.newegg.ec.redis.entity.AnalyzeStatus;
 import com.newegg.ec.redis.service.RDBAnalyzeService;
 import com.newegg.ec.redis.utils.Report;
@@ -14,8 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.server.ConfigurableWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -26,7 +23,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Analyzer implements CommandLineRunner ,WebServerFactoryCustomizer<ConfigurableWebServerFactory>  {
+public class Analyzer implements CommandLineRunner {
   private final static Logger LOG = LoggerFactory.getLogger(Analyzer.class);
 
   @Value("${rct.elasticsearch.rest.urls}")
@@ -93,37 +90,16 @@ public class Analyzer implements CommandLineRunner ,WebServerFactoryCustomizer<C
     KEY_PREFIX_SEPARATORS = this.keyPrefixSeparators;
     
     // 从configMap中获取参数	(使用时放开以下注释，同时注释掉以上代码)
-//    if(Boolean.valueOf(configMap.get(AnalyzerConstant.RCT_ELASTICSEARCH_ENABLE))) {
-//    	ElasticSearchUtil.init(30000, 30000, configMap.get(AnalyzerConstant.RCT_ELASTICSEARCH_REST_URLS));
+//    if(Boolean.valueOf(System.getenv().get(AnalyzerConstant.RCT_ELASTICSEARCH_ENABLE))) {
+//    	ElasticSearchUtil.init(30000, 30000, System.getenv().get(AnalyzerConstant.RCT_ELASTICSEARCH_REST_URLS));
 //    }
-//    Report.init(30000, 30000, configMap.get(AnalyzerConstant.EUREKA_CLIENT_SERVICEURL_DEFAULTZONE));
-//    ESINDEX = Analyzer.configMap.get(AnalyzerConstant.RCT_ELASTICSEARCH_INDEX);
-//    FILTER_Bytes = Long.valueOf(Analyzer.configMap.get(AnalyzerConstant.RCT_FILTER_BYTES));
-//    FILTER_ItemCount = Integer.valueOf(Analyzer.configMap.get(AnalyzerConstant.RCT_FILTER_ITEM_COUNT));
-//    MAX_EsSenderThreadNum = Integer.valueOf(Analyzer.configMap.get(AnalyzerConstant.RCT_MAX_ES_THREAD_NUM));
+//    Report.init(30000, 30000, System.getenv().get(AnalyzerConstant.EUREKA_CLIENT_SERVICEURL_DEFAULTZONE));
+//    ESINDEX = System.getenv().get(AnalyzerConstant.RCT_ELASTICSEARCH_INDEX);
+//    FILTER_Bytes = Long.valueOf(System.getenv().get(AnalyzerConstant.RCT_FILTER_BYTES));
+//    FILTER_ItemCount = Integer.valueOf(System.getenv().get(AnalyzerConstant.RCT_FILTER_ITEM_COUNT));
+//    MAX_EsSenderThreadNum = Integer.valueOf(System.getenv().get(AnalyzerConstant.RCT_MAX_ES_THREAD_NUM));
 //    MAX_QUEUE_SIZE = 1000;
-//    KEY_PREFIX_INDEX_LOCATION = Analyzer.configMap.get(AnalyzerConstant.RCT_ANALYZE_KEY_PREFIX_INDEX_LOCATION);
-//    KEY_PREFIX_SEPARATORS = Analyzer.configMap.get(AnalyzerConstant.RCT_ANALYZE_KEY_PREFIX_SEPARATORS);
-  }
-
-  // 从环境变量中获取所有参数，给configMap赋值
-  @Override
-  public void customize(ConfigurableWebServerFactory factory) {
-	configMap.put(AnalyzerConstant.SERVER_PORT, System.getenv().get(AnalyzerConstant.SERVER_PORT));
-	configMap.put(AnalyzerConstant.RCT_MAX_ES_THREAD_NUM, System.getenv().get(AnalyzerConstant.RCT_MAX_ES_THREAD_NUM));
-	configMap.put(AnalyzerConstant.RCT_FILTER_ITEM_COUNT, System.getenv().get(AnalyzerConstant.RCT_FILTER_ITEM_COUNT));
-	configMap.put(AnalyzerConstant.RCT_FILTER_BYTES, System.getenv().get(AnalyzerConstant.RCT_FILTER_BYTES));
-	configMap.put(AnalyzerConstant.RCT_ELASTICSEARCH_ENABLE, System.getenv().get(AnalyzerConstant.RCT_ELASTICSEARCH_ENABLE));
-	configMap.put(AnalyzerConstant.RCT_ELASTICSEARCH_REST_URLS, System.getenv().get(AnalyzerConstant.RCT_ELASTICSEARCH_REST_URLS));
-	configMap.put(AnalyzerConstant.RCT_ELASTICSEARCH_INDEX, System.getenv().get(AnalyzerConstant.RCT_ELASTICSEARCH_INDEX));
-	configMap.put(AnalyzerConstant.RCT_ANALYZE_KEY_PREFIX_SEPARATORS, System.getenv().get(AnalyzerConstant.RCT_ANALYZE_KEY_PREFIX_SEPARATORS));
-	configMap.put(AnalyzerConstant.RCT_ANALYZE_KEY_PREFIX_INDEX_LOCATION, System.getenv().get(AnalyzerConstant.RCT_ANALYZE_KEY_PREFIX_INDEX_LOCATION));
-	configMap.put(AnalyzerConstant.EUREKA_CLIENT_SERVICEURL_DEFAULTZONE, System.getenv().get(AnalyzerConstant.EUREKA_CLIENT_SERVICEURL_DEFAULTZONE));
-	configMap.put(AnalyzerConstant.EUREKA_INSTANCE_PREFERIPADDRESS, System.getenv().get(AnalyzerConstant.EUREKA_INSTANCE_PREFERIPADDRESS));
-	configMap.put(AnalyzerConstant.EUREKA_INSTANCE_LEASE_RENEWAL_NTERVAL_IN_SECONDS, System.getenv().get(AnalyzerConstant.EUREKA_INSTANCE_LEASE_RENEWAL_NTERVAL_IN_SECONDS));
-	configMap.put(AnalyzerConstant.EUREKA_INSTANCE_LEASE_EXPIRATION_DURATION_IN_SECONDS, System.getenv().get(AnalyzerConstant.EUREKA_INSTANCE_LEASE_EXPIRATION_DURATION_IN_SECONDS));
-	configMap.put(AnalyzerConstant.LOGGING_LEVEL_COM_NETFLIX_EUREKA, System.getenv().get(AnalyzerConstant.LOGGING_LEVEL_COM_NETFLIX_EUREKA));
-	configMap.put(AnalyzerConstant.LOGGING_LEVEL_COM_NETFLIX_DISCOVERY, System.getenv().get(AnalyzerConstant.LOGGING_LEVEL_COM_NETFLIX_DISCOVERY));
-	configMap.put(AnalyzerConstant.SPRING_APPLICATION_NAME, System.getenv().get(AnalyzerConstant.SPRING_APPLICATION_NAME));
+//    KEY_PREFIX_INDEX_LOCATION = System.getenv().get(AnalyzerConstant.RCT_ANALYZE_KEY_PREFIX_INDEX_LOCATION);
+//    KEY_PREFIX_SEPARATORS =System.getenv().get(AnalyzerConstant.RCT_ANALYZE_KEY_PREFIX_SEPARATORS);
   }
 }
