9e8e85b706f1a1f7aacdc17eab4a53b73b6387a8
weitienwong@outlook.com <weitienwong@outlook.com>
2022-04-01 17:25:03 +0800

    refactor: 配置数据库连接

--- redis-manager-dashboard/src/main/java/com/newegg/ec/redis/RedisManagerApplication.java
+++ redis-manager-dashboard/src/main/java/com/newegg/ec/redis/RedisManagerApplication.java
@@ -1,6 +1,8 @@
 package com.newegg.ec.redis;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -21,7 +23,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableEurekaServer
 @SpringBootApplication
 public class RedisManagerApplication {
-
+    private final Logger log = LoggerFactory.getLogger(RedisManagerApplication.class);
+    private String datasourceUrl;
     public static void main(String[] args) {
         SpringApplication.run(RedisManagerApplication.class);
     }
